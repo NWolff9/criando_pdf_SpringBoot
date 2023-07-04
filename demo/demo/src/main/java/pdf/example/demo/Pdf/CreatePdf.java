9fd44fdf68478284f42856cd1e59a696b9ef1e18
WON1CA <natha.wolff@br.bosch.com>
2023-07-04 08:11:11 -0300

    tabela

--- demo/demo/src/main/java/pdf/example/demo/Pdf/CreatePdf.java
+++ demo/demo/src/main/java/pdf/example/demo/Pdf/CreatePdf.java
@@ -1,31 +1,18 @@
 package pdf.example.demo.Pdf;
 
-import com.lowagie.text.Document;
-import com.lowagie.text.Paragraph;
+import com.lowagie.text.*;
+import com.lowagie.text.pdf.PdfCell;
+import com.lowagie.text.pdf.PdfPCell;
+import com.lowagie.text.pdf.PdfPTable;
 import com.lowagie.text.pdf.PdfWriter;
 
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.IOException;
 
 public class CreatePdf {
     public CreatePdf(String frase){
-        Document document = new Document();
 
-        try {
-            PdfWriter.getInstance(document, new FileOutputStream("relatorio.pdf"));
-
-            document.open();
-
-            Paragraph paragrafoTeste = new Paragraph(frase);
-
-            document.add(paragrafoTeste);
-
-            document.close();
-
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException(e);
-        }
-    }
 
 
 }
